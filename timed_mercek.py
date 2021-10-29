4c35dac84b4475feec576be632ec1df5a1bffb3e
Yusuf Saraçlıoğlu <yusufsaraclioglu@gmail.com>
2021-10-29 16:22:13 +0300

    Revert "Stealthy"
    This reverts commit 2309eef76273052de5f5e9250003d7f493efed47.

--- timed_mercek.py
+++ timed_mercek.py
@@ -1,14 +1,16 @@
+from subprocess import Popen
 from pwinput import pwinput as pw
 import schedule
 import random
 from mercek import *
 
-# Kullanıcı adı, şifre farz oldu.
 username = input("Kullanıcı adınız: ")
 password = pw("Şifre: ")
 bilgiler(username, password)
 
-# Asıl olayımız
+a = random.randint(a=10, b=30)
+
+
 def kararmercigi():
     login()
     rutinal()
@@ -16,11 +18,7 @@ def kararmercigi():
     quit()
 
 
-# Schedule kütüphanesi ile zamanlama yaptık.
+schedule.every().hour.at(f'20:{a}').do(kararmercigi)
 
 while 1:
-    a = random.randint(a=10, b=30)
-    # Belli olmayak diye de random vakitlerde attık.
-    schedule.every().hour.at(f'20:{a}').do(kararmercigi)
-    if schedule.run_pending() == None:
-        continue
+    schedule.run_pending()
